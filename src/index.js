76aa951c513f550f6d8da5af0eca9cf669abec2a
karapuce <typhaine.demangeon@gmail.com>
2020-07-30 11:42:49 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Contact from './components/Contact';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Contact />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
